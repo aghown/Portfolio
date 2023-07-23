d5f3d9997188bfad2ab1f9081b2da71ded5132ac
Hanna Silva <hannasilva696@gmail.com>
2023-07-23 12:23:09 -0300

    commit responsivo
    responsivo

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-function ativarLetra(elemento){
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((letra, i)=> {
-        setTimeout(()=>{
-            elemento.innerHTML += letra;
-        }, 75 * i);
-    });
 
+function escrevendoLetra(){
+    function ativarLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((letra, i)=> {
+            setTimeout(()=>{
+                elemento.innerHTML += letra;
+            }, 75 * i);
+        });
+    
+    }
+    
+    const titulo = document.querySelector('.digitando');
+    ativarLetra(titulo);
 }
 
-const titulo = document.querySelector('.digitando');
-ativarLetra(titulo);
+const ativaMenu = document.querySelector('.fa-bars')
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+})
